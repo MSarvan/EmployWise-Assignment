e7b02b81256a61f8a0cb1d2838c92019e5f626a5
Saravanan M <sarvan.m20@gmail.com>
2025-03-29 13:19:29 +0530

    updated user's page UI

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import "../styles/User.scss";
 
 const User = () => {
   return (
-    <div>User</div>
-  )
-}
+    <div className="userpage">
+      <Navbar />
+      <div className="user-container">
+        <h2>Edit User's Data</h2>
+        <form action="">
+          <div>
+            <label htmlFor="First Name">First Name</label>
+            <input
+              type="text"
+              // onChange={(e) => {
+              //   setPassword(e.target.value);
+              // }}
+              // value={password}
+            />
+          </div>
+          <div>
+            <label htmlFor="Last Name">Last Name</label>
+            <input
+              type="text"
+              // onChange={(e) => {
+              //   setPassword(e.target.value);
+              // }}
+              // value={password}
+            />
+          </div>
+          <div>
+            <label htmlFor="Email">Email</label>
+            <input
+              type="email"
+              // onChange={(e) => {
+              //   setPassword(e.target.value);
+              // }}
+              // value={password}
+            />
+          </div>
+          <button className="update-button">Update details</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default User
+export default User;
